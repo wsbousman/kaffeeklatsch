02a421b8b3528232499e41547e32214d568cfcaa
wsbousman <85909621+wsbousman@users.noreply.github.com>
2024-01-24 04:55:01 -0600

    mobile fixes
    text align property used to fix uncentered <p> in mobile view, padding in media queries, much to do but its late...

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <div className="line"></div>
         <div className="wrapper">
           <p className="blurb">
-            Today, I got coffee with my friend Jake. 
+            Today, I got coffee with my friend Jake. He mentioned a friend who was working in UI/UX design, and that this friend had spent the last week writing a total of three lines of CSS. I didnt intend to come off as overconfident or entitled- but I may have said something to the effect of being able to manage that myself. Perhaps even four lines a week. Following coffee, I built and deployed this portfolio of mockups to showcase my frontend design abilities. Feel free to peruse them and, if you're interested, contact me. Thanks for looking.
           </p>
         </div>
       </div>
